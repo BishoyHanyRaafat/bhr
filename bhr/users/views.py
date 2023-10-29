13e57c737c8c74dbbd87e1cebc300d37f6764f7d
BishoyHanyRaafat <87083879+BishoyHanyRaafat@users.noreply.github.com>
2023-10-29 16:27:58 +0200

    fixed the ip issue

--- bhr/users/views.py
+++ bhr/users/views.py
@@ -6,21 +6,19 @@ from django.contrib.auth.models import User, Group
 from .models import IPAddress
 from django.http import HttpResponse
 # Create your views here.
-
 def home(request):
     user = request.user
-    try:
-        user.id
-    except:
-        return render(request, 'home.html')
-    if user:
+
+    if user.is_authenticated:
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             user_ip = x_forwarded_for.split(',')[-1].strip()
         else:
             user_ip = request.META.get('REMOTE_ADDR')
-            ip, _ = IPAddress.objects.get_or_create(address=user_ip)
+        
+        ip, _ = IPAddress.objects.get_or_create(address=user_ip)
         ip.users.add(user)
+    
     return render(request, 'home.html')
 
 def sign_up(request):
