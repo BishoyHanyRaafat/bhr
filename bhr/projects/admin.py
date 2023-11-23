c2c5ae17e4243768467bb9267f0f14d5351135bd
BishoyHanyRaafat <87083879+BishoyHanyRaafat@users.noreply.github.com>
2023-11-23 02:19:20 +0200

    Update admin.py

--- bhr/projects/admin.py
+++ bhr/projects/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import Project, ProjectImage, Certificate
 # Register your models here.
-
-admin.site.register(Project)
-admin.site.register(ProjectImage)
-admin.site.register(Certificate)
 class ImageInline(admin.TabularInline):  # or admin.StackedInline
     model = ProjectImage
     extra = 2  # the number of empty forms to display
 
 class ProjectAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectImage)
+admin.site.register(Certificate)
