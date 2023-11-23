a92de772755519f349579ed0b6777f2886c86692
BishoyHanyRaafat <bishoyhany006@gmail.com>
2023-11-23 02:17:28 +0200

    added the images inline admin

--- bhr/projects/admin.py
+++ bhr/projects/admin.py
@@ -5,3 +5,9 @@ from .models import Project, ProjectImage, Certificate
 admin.site.register(Project)
 admin.site.register(ProjectImage)
 admin.site.register(Certificate)
+class ImageInline(admin.TabularInline):  # or admin.StackedInline
+    model = ProjectImage
+    extra = 2  # the number of empty forms to display
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
